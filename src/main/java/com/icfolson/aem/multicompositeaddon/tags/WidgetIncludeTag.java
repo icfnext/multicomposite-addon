2d0ff834384b375c06adbff78b0c5056f3c871f5
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-06-25 13:57:53 -0500

    updated for compatibility with granite form data changes in 6.3sp2/6.4

--- src/main/java/com/icfolson/aem/multicompositeaddon/tags/WidgetIncludeTag.java
+++ src/main/java/com/icfolson/aem/multicompositeaddon/tags/WidgetIncludeTag.java
@@ -2,15 +2,19 @@ package com.icfolson.aem.multicompositeaddon.tags;
 
 import com.adobe.granite.ui.components.ComponentHelper;
 import com.adobe.granite.ui.components.ComponentHelper.Options;
-import com.adobe.granite.ui.components.Value;
+import com.adobe.granite.ui.components.FormData;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class WidgetIncludeTag extends TagSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WidgetIncludeTag.class);
 
     private ValueMap valueMap;
 
@@ -20,21 +24,26 @@ public class WidgetIncludeTag extends TagSupport {
 
     @Override
     public int doEndTag() throws JspException {
-        SlingHttpServletRequest request = (SlingHttpServletRequest) pageContext.getAttribute("slingRequest");
-        ComponentHelper cmp = (ComponentHelper) pageContext.getAttribute("cmp");
-
-        ValueMap existing = (ValueMap) request.getAttribute(Value.FORM_VALUESS_ATTRIBUTE);
-        String existingPath = (String) request.getAttribute(Value.CONTENTPATH_ATTRIBUTE);
+        final SlingHttpServletRequest request = (SlingHttpServletRequest) pageContext.getAttribute("slingRequest");
+        final ComponentHelper cmp = (ComponentHelper) pageContext.getAttribute("cmp");
 
         if (valueMap == null) {
-            request.removeAttribute(Value.FORM_VALUESS_ATTRIBUTE);
-            request.removeAttribute(Value.CONTENTPATH_ATTRIBUTE);
+            FormData.push(request, ValueMap.EMPTY, FormData.NameNotFoundMode.IGNORE_FRESHNESS);
         } else {
-            request.setAttribute(Value.FORM_VALUESS_ATTRIBUTE, valueMap);
+            final FormData formData = FormData.from(request);
+
+            FormData.NameNotFoundMode mode = FormData.NameNotFoundMode.IGNORE_FRESHNESS;
+
+            if (formData != null) {
+                mode = formData.getMode();
+            }
+
+            FormData.push(request, valueMap, mode);
         }
 
         try {
-            Resource field = request.getResourceResolver().getResource(path);
+            final Resource field = request.getResourceResolver().getResource(path);
+
             if (readOnly) {
                 cmp.include(field, cmp.getReadOnlyResourceType(field), new Options().rootField(false));
             } else {
@@ -44,8 +53,7 @@ public class WidgetIncludeTag extends TagSupport {
             throw new JspException("Error including component", e);
         }
 
-        request.setAttribute(Value.FORM_VALUESS_ATTRIBUTE, existing);
-        request.setAttribute(Value.CONTENTPATH_ATTRIBUTE, existingPath);
+        FormData.pop(request);
 
         return EVAL_PAGE;
     }
